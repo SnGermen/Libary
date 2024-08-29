0e5643c551bcc4872ecec9031183f856bb3492b8
MikiMouse <songagermen8@gmail.com>
2024-08-29 05:10:19 +0200

    madre mia

--- src/app.js
+++ src/app.js
@@ -1,28 +1,25 @@
-import "./app.css"
-import { MainView } from './views/main/main'
-document.addEventListener('DOMContentLoaded', () => {
-  class App {
-    routes = [
-      { path: "", view: MainView }
-    ]
 
-    constructor() {
-      window.addEventListener("hashchange", this.route.bind(this)) // Исправлено: rote -> route
-    }
+import { MainView } from './views/main.js'
+class App {
+  routes = [
+    { path: "", view: MainView }
+  ]
 
-    route() { // Исправлено: rote -> route
-      if (this.currentView) {
-        this.currentView.destroy()
-      }
-      const view = this.routes.find(r => r.path == location.hash).view
-      this.currentView = new view()
-      this.currentView.render()
-    }
+  constructor() {
+    window.addEventListener("hashchange", this.route.bind(this))
+    this.route
   }
 
-  console.log('aaaa')
+  route() { // Исправлено: rote -> route
+    if (this.currentView) {
+      this.currentView.destroy()
+    }
+    const view = this.routes.find(r => r.path == location.hash).view
+    this.currentView = new view()
+    this.currentView.render()
+  }
+}
 
-  new App()
 
-
-})
+new App()
+console.log('aaaa')
--- src/views/main.js
+++ src/views/main.js
@@ -1,10 +1,12 @@
-import { AbstractiveView } from '../../common/view';
+import { AbstractiveView } from '../../common/view.js';
 
 export class MainView extends AbstractiveView {
   constructor() {
     super()
     this.setTitle("Book Search")
   }
+
+
   render() {
     const main = document.createElement("div");
     main.innerHTML = "Teast"; // Возможно, вы хотели написать "Test"
